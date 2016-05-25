5195e653b9081f4f4405f9608b8c65a5ecde3446
Igor Gurovich <igor.gurovich@gmail.com>
2016-05-25 09:59:08 -0700

    Adding files for multi-vm setup.sh

--- scripts/centos-web.sh
+++ scripts/centos-web.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# Update CentOS with any patches
-yum update -y --exclude=kernel
-
-# Tools
-yum install -y nano git unzip screen
-
-
 # Apache
 yum install -y httpd httpd-devel httpd-tools
-
 chkconfig --add httpd
 chkconfig httpd on
 service httpd stop
@@ -22,17 +14,8 @@ service httpd start
 # PHP
 yum install -y php php-cli php-common php-devel php-mysql
 
-
-# MySQL
-yum install -y mysql mysql-server mysql-devel
-chkconfig --add mysqld
-chkconfig mysqld on
-service mysqld start
-
-mysql -u root -e "SHOW DATABASES";
-
 #Download Starter Content
-cd /vagrant 
+cd /vagrant
 sudo su - vagrant wget https://raw.githubusercontent.com/igurovich/vagrant/master/files/index.html
 sudo su - vagrant wget https://raw.githubusercontent.com/igurovich/vagrant/master/files/info.php
 
